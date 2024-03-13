f47224ca44ee44c4fe32a09cf7c2d82574d8cd98
Aliaksei Karzhou <alexius90e@gmail.com>
2024-03-13 13:51:54 +0300

    feat: added mobile nav

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 import './custom-select.js';
 
+const headerBurger = document.querySelector('.header__burger');
+const headerNav = document.querySelector('.header__nav');
+
+if (headerBurger && headerNav) {
+  headerBurger.addEventListener('click', (event) => {
+    headerBurger.classList.toggle('active');
+    headerNav.classList.toggle('active');
+  });
+
+  headerNav.addEventListener('click', (event) => {
+    if (event.target.classList.contains('close')) {
+      event.currentTarget.classList.remove('active');
+      headerBurger.classList.remove('active');
+    }
+  });
+}
+
 const themeTogglers = document.querySelectorAll('.theme-toggler');
 
 themeTogglers.forEach((themeToggler) => {
   themeToggler.addEventListener('click', () => {
-    themeToggler.classList.toggle('theme-toggler_dark');
+    themeTogglers.forEach((toggler) => toggler.classList.toggle('theme-toggler_dark'));
     document.body.classList.toggle('dark');
   });
 });
